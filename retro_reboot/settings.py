6fd1505dc58d663921f9a556e2294bc997a4c02c
Adam <adamgilroy22@gmail.com>
2023-05-08 21:59:13 +0100

    Add root and allauth template directories to settings

--- retro_reboot/settings.py
+++ retro_reboot/settings.py
@@ -63,7 +63,10 @@ ROOT_URLCONF = 'retro_reboot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
