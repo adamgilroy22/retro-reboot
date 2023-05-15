14a1c34b754de81ca5f6b9000109d4c0ab5f2c96
Adam <adamgilroy22@gmail.com>
2023-05-15 09:27:49 +0100

    Create and migrate product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    platform = models.CharField(max_length=254)
+    year = models.IntegerField()
+    description = models.TextField()
+    condition = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
