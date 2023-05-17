f009504a33bab40cc79f560c7802e2d164a0dace
Adam <adamgilroy22@gmail.com>
2023-05-17 09:55:16 +0100

    Register Platform and Condition classes in admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import *
 
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Platform)
+admin.site.register(Condition)
