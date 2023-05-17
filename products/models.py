8ca01752a71bfce91db56c7f662531948232079b
Adam <adamgilroy22@gmail.com>
2023-05-17 09:27:13 +0100

    Create Condition class in products models

--- products/models.py
+++ products/models.py
@@ -15,13 +15,20 @@ class Platform(models.Model):
         return self.name
     
 
+class Condition(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+    
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     platform = models.ForeignKey('Platform', null=True, blank=True, on_delete=models.SET_NULL)
     year = models.IntegerField()
     description = models.TextField()
-    condition = models.CharField(max_length=254)
+    condition = models.ForeignKey('Condition', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
