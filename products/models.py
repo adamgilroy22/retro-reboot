17efd0c07fbc3ee4f2c4b9d0949276ce165febd6
Adam <adamgilroy22@gmail.com>
2023-05-17 09:26:21 +0100

    Create Platform class in products models

--- products/models.py
+++ products/models.py
@@ -6,12 +6,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
     
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    platform = models.CharField(max_length=254)
+    platform = models.ForeignKey('Platform', null=True, blank=True, on_delete=models.SET_NULL)
     year = models.IntegerField()
     description = models.TextField()
     condition = models.CharField(max_length=254)
