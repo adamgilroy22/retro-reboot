d9587f78c8636809dc88c0b826179531d9aa675c
Adam <adamgilroy22@gmail.com>
2023-05-22 12:55:07 +0100

    Fix typo in products views context

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ def all_products(request):
     products = Product.objects.all()
 
     context = {
-        'products', products,
+        'products': products,
     }
     
     return render(request, 'products/products.html', context)
