1f652d3a8ef54d33204fdeb3bb5bc8b88395cb1f
Adam <adamgilroy22@gmail.com>
2023-05-22 08:23:09 +0100

    Set verbose_name_plural for Category and Condition classes

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
 
     def __str__(self):
@@ -16,6 +20,10 @@ class Platform(models.Model):
     
 
 class Condition(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Condition'
+
     name = models.CharField(max_length=254)
 
     def __str__(self):
