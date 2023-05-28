faa506f1ba3f8d553f3ee32ba527dbd18513a860
Adam Gilroy <adamgilroy22@gmail.com>
2023-05-28 10:32:05 +0000

    Include localhost in ALLOWED_HOSTS

--- retro_reboot/settings.py
+++ retro_reboot/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['retro-reboot.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['retro-reboot.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
