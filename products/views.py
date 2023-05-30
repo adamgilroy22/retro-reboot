8597a7466f1461cc21244939d4224dc8432f7778
Adam Gilroy <adamgilroy22@gmail.com>
2023-05-30 11:20:02 +0000

    Add sortkey logic for platform

--- products/views.py
+++ products/views.py
@@ -26,6 +26,8 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
+            if sortkey == 'platform':
+                sortkey = 'platform__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
