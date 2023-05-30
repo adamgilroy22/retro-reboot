553dcf4df20aec75fdbf4a06313f3811dfa3b49d
Adam Gilroy <adamgilroy22@gmail.com>
2023-05-30 13:30:14 +0000

    Add basket urls path to project level urls.py

--- retro_reboot/urls.py
+++ retro_reboot/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('homepage.urls')),
     path('products/', include('products.urls')),
+    path('basket/', include('basket.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
