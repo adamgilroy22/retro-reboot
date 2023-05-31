ae2fe4a4c046045750d769de521b7114716ba1d3
Adam Gilroy <adamgilroy22@gmail.com>
2023-05-31 19:18:04 +0000

    Loop over products in basket to get total

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,8 +1,23 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
 
     context = {
         'basket_items': basket_items,
