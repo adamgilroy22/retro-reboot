ba91be74f850bd4934a1e01d4e6f875d278d4cb5
Adam Gilroy <adamgilroy22@gmail.com>
2023-05-31 22:39:33 +0000

    Add url path for adjusting basket

--- basket/urls.py
+++ basket/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
 ]
