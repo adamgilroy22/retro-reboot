7d8c407742157751f6f01e1a8a624c52d8ffd5d9
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-01 15:36:02 +0000

    Add error message to remove_from_basket view

--- basket/views.py
+++ basket/views.py
@@ -65,4 +65,5 @@ def remove_from_basket(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
