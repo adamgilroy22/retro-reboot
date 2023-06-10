bc9c89e79f03a666ea0018d6e2e03f3ff5871c68
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-10 20:00:59 +0000

    Register discount code to admin

--- discount_codes/admin.py
+++ discount_codes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+    ordering = ('discount',)
+
+
+admin.site.register(DiscountCode, DiscountCodeAdmin)
