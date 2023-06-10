6b9d33ae22a39ec9789d2fa940b819085884554f
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-10 13:08:46 +0000

    Create DiscountCode model

--- discount_codes/models.py
+++ discount_codes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class DiscountCode(models.Model):
+    code = models.CharField(max_length=20, null=False,
+                            blank=False, unique=True)
+    discount = models.IntegerField(null=False, blank=False)
