62b7f2fe0142ab794aa92cd99e52127c0dbbf1b4
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-10 19:58:49 +0000

    Add validators to discount field in DiscountCode model

--- discount_codes/models.py
+++ discount_codes/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class DiscountCode(models.Model):
     code = models.CharField(max_length=20, null=False,
                             blank=False, unique=True)
-    discount = models.IntegerField(null=False, blank=False)
+    discount = models.IntegerField(null=False, blank=False, validators=[
+            MaxValueValidator(99),
+            MinValueValidator(1)
+        ])
