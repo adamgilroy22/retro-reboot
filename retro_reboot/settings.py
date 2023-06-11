641ed34d74523f7c008f71a678ebfff56052d97b
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-11 10:15:46 +0000

    Update gitpod link in allowed hosts

--- retro_reboot/settings.py
+++ retro_reboot/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['retro-reboot.herokuapp.com', 'localhost', '127.0.0.1', '8000-adamgilroy2-retroreboot-ct5dzv9txy5.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['retro-reboot.herokuapp.com', 'localhost', '127.0.0.1', '8000-adamgilroy2-retroreboot-ct5dzv9txy5.ws-eu99.gitpod.io']
 
 
 # Application definition
