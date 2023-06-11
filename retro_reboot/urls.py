3faa89f8aee9a1a4f2765edb388bad150decc228
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-11 14:53:47 +0000

    Add checkout urls to project urls

--- retro_reboot/urls.py
+++ retro_reboot/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
