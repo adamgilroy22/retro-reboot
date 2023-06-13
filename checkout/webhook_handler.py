12c0ef64b1be2be392c6a4925d3dd873bb6710e1
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-13 18:52:41 +0000

    Set user_profile to none if user isn't logged in

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -88,6 +88,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
