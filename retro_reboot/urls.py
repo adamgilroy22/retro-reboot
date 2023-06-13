dff41aea6304f00a242f7793e2fbdb4cb5454b41
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-13 14:17:25 +0000

    Add profile urls to project level urls

--- retro_reboot/urls.py
+++ retro_reboot/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
