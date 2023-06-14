90e3ff587db490f63a8043b9d64d58d7ef8b0090
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-14 15:02:53 +0000

    Update webhook_handler to get the charge object

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -51,6 +51,11 @@ class StripeWH_Handler:
         basket = intent.metadata.basket
         save_info = intent.metadata.save_info
 
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
