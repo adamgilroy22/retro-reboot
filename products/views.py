50fe493fee6b4ad7a1493a2eabf0ce0f4f5241a4
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-14 11:43:29 +0000

    Add function to delete products on the front end

--- products/views.py
+++ products/views.py
@@ -132,3 +132,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
