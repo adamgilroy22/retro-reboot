2a8a9ff6cdc93d4e32c8becb6ccd67707df3181e
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-14 15:52:50 +0000

    Move confirmation email function to checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
     )
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -159,6 +161,23 @@ def checkout_success(request, order_number):
             if user_profile_form.is_valid():
                 user_profile_form.save()
 
+    def _send_confirmation_email(order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
@@ -171,4 +190,5 @@ def checkout_success(request, order_number):
         'order': order,
     }
 
+    _send_confirmation_email(order)
     return render(request, template, context)
