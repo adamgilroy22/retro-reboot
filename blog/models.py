bca9b370f0c1246b446411829a448174bedf1a78
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-21 07:49:06 +0000

    Create Post model in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = ImageField('image', default='placeholder')
+    excerpt = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
