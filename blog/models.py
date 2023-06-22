68575eeb922f9f2b67811a84970b59be3c2758ae
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-22 08:40:26 +0000

    Create comment model

--- blog/models.py
+++ blog/models.py
@@ -23,3 +23,19 @@ class Post(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
