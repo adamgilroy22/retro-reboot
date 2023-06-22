f73eb5adb09dea96d49ec0ee6d08de5e79b6b5b6
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-22 13:55:27 +0000

    Fix issue in PostList queryset swap status for published

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(published=True).order_by('-created_on')
     template_name = 'blog/blog.html'
     paginate_by = 6
