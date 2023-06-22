f6eb564239825b963b3b1f7e07eb1c506975296b
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-22 14:51:09 +0000

    Set comments for automatic approval

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['created_on']
