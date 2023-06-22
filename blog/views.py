7e970ce04359d421f904dcfdf24c58e6e17db4d7
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-22 14:16:44 +0000

    Create PostLike view

--- blog/views.py
+++ blog/views.py
@@ -63,3 +63,15 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+
+class PostLike(View):
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
