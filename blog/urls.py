3d5fd0d1804276dcbf1c444e7c4a3ffeaa66b80c
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-22 14:16:58 +0000

    Add url for liking posts in blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
