3415214d93fbf2684ab22e3338aa8f70a80d6e17
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-23 12:54:11 +0000

    Add PostForm to blog forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,25 @@
-from .models import Comment
+from .models import Comment, Post
+from products.widgets import CustomClearableFileInput
 from django import forms
 
 
+class PostForm(forms.ModelForm):
+    """
+    Create a new post using Post model
+    """
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'author', 'content',
+                  'excerpt', 'featured_image', 'published',)
+
+    featured_image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
+
+
 class CommentForm(forms.ModelForm):
+    """
+    Create a new comment using Comment model
+    """
     class Meta:
         model = Comment
         fields = ('body',)
