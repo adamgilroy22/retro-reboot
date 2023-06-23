cc7bc8df379de60ba140196324849de8279f2c23
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-23 12:57:23 +0000

    Add url for adding new post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('post/add/', views.add_post, name='add_post'),
 ]
