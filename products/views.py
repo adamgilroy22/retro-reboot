721bb4358c61aa588df3e81289e9a6a8fb88ae8d
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-23 19:19:38 +0000

    Safeguard product deletion with confirmation page

--- products/views.py
+++ products/views.py
@@ -146,6 +146,13 @@ def delete_product(request, product_id):
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
-    product.delete()
-    messages.success(request, 'Product deleted!')
-    return redirect(reverse('products'))
+    if request.method == 'POST':
+        product.delete()
+        return redirect(reverse('products'))
+
+    template = 'products/delete_product.html'
+    context = {
+        'product': product,
+    }
+
+    return render(request, template, context)
