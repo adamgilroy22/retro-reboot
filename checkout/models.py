7fd87d9b653e65b6f93032bd826d198c12bc24bc
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-26 19:08:02 +0000

    Change discount code in Order model to ForeignKey

--- checkout/models.py
+++ checkout/models.py
@@ -25,7 +25,9 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    discount_code = models.CharField(max_length=20, null=True, blank=True)
+    discount_code = models.ForeignKey(
+        'DiscountCode', on_delete=models.SET_NULL, max_length=20,
+        null=True, blank=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(
