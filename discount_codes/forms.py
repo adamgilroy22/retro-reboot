f55ddc77993701d645adce0cb919c16e2ee85dcc
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-26 19:11:27 +0000

    Add DiscountForm

---
+++ discount_codes/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import DiscountCode
+
+
+class DiscountForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Discount Code',
+    }))
