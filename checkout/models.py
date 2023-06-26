fbd6f42f6d49f00c8d8767e38bffc68993dff133
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-26 19:26:08 +0000

    Remove '' around DiscountCode in discount_code foreign key

--- checkout/models.py
+++ checkout/models.py
@@ -26,7 +26,7 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     discount_code = models.ForeignKey(
-        'DiscountCode', on_delete=models.SET_NULL, max_length=20,
+        DiscountCode, on_delete=models.SET_NULL, max_length=20,
         null=True, blank=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
