75540b0db2ee931d9fe245ab5c604659a41e0117
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-27 19:15:41 +0000

    Move all contact page code into contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def contact(request):
+    """
+    A view to return the index page
+    """
+    return render(request, 'contact/contact.html')
--- homepage/views.py
+++ homepage/views.py
@@ -6,10 +6,3 @@ def index(request):
     A view to return the index page
     """
     return render(request, 'homepage/index.html')
-
-
-def contact(request):
-    """
-    A view to return the index page
-    """
-    return render(request, 'homepage/contact.html')
