b5d670a9f85f2c325108b226785ac2b588a12abf
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-27 18:57:35 +0000

    Create view to load contact page

--- homepage/views.py
+++ homepage/views.py
@@ -6,3 +6,10 @@ def index(request):
     A view to return the index page
     """
     return render(request, 'homepage/index.html')
+
+
+def contact(request):
+    """
+    A view to return the index page
+    """
+    return render(request, 'homepage/contact.html')
