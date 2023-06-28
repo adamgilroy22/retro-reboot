61645317a3649a41f4685ef234068ceeff8f7f64
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-28 15:26:16 +0000

    Require email to be unique in NewsletterSignup model

--- newsletter/models.py
+++ newsletter/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class NewsletterSignup(models.Model):
-    email = models.EmailField(null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
 
     def __str__(self):
         return self.email
