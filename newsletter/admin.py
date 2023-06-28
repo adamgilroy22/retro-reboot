c3e9900e7f1d7f15186a56fc6d48a75e5dc0c48a
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-28 13:40:12 +0000

    Register newsletter sign ups in admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class NewsletterMemberAdmin(admin.ModelAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(NewsletterSignup, NewsletterMemberAdmin)
