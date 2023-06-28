1d37dfaf94dc621e9d03bec681c07fe8a45e6005
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-28 13:40:27 +0000

    Create newsletter_signup view

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import NewsletterSignup
+from .forms import NewsletterForm
 
-# Create your views here.
+
+def newsletter_signup(request):
+    """ Join Newsletter mailing list """
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            member = form.save()
+            messages.info(request, 'Successfully joined our mailing list!')
+            return redirect(request.META.get('HTTP_REFERER', '/'))
+        else:
+            messages.error(request, 'Failed to sign up. Please ensure the \
+            form is valid.')
+    else:
+        form = NewsletterForm()
+
+    context = {
+        'newsletter_form': form,
+    }
+
+    return redirect(request.META.get('HTTP_REFERER', '/'))
