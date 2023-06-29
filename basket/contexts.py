05fed74bf350e61f32e0f75c908796b80542bc7a
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-29 09:51:57 +0000

    Adjust basket contexts for delivery

--- basket/contexts.py
+++ basket/contexts.py
@@ -19,10 +19,23 @@ def basket_contents(request):
                 'product': product,
             })
 
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
     context = {
         'basket_items': basket_items,
-        'grand_total': total,
+        'total': total,
         'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
