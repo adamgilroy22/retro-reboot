d4a55c266749a0fbb2647d0abbcf47493973d256
Adam Gilroy <adamgilroy22@gmail.com>
2023-06-29 12:50:22 +0000

    Configure handler404 url

--- retro_reboot/urls.py
+++ retro_reboot/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +32,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'retro_reboot.views.handler404'
