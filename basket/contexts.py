60af93db707807651faa9dbf024dd72a4cb61cd0
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-01 15:53:32 +0000

    Save discount to request.session on form submit

--- basket/contexts.py
+++ basket/contexts.py
@@ -27,11 +27,13 @@ def basket_contents(request):
     product_count = 0
     discount_code = 0
     basket = request.session.get('basket', {})
+    discount = request.session.get('discount')
 
     discount_form = DiscountForm(request.POST or None)
     if discount_form.is_valid():
         code = discount_form.cleaned_data.get('code')
         discount_code = get_discount_code(request, code)
+        request.session['discount'] = discount_code
 
     for item_id, quantity in basket.items():
         product = get_object_or_404(Product, pk=item_id)
@@ -50,7 +52,10 @@ def basket_contents(request):
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total - discount_code
+    grand_total = delivery + total
+
+    if discount:
+        grand_total -= discount
 
     context = {
         'basket_items': basket_items,
