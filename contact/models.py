85cd2d3e29161e7a5743af4fdad93df2d2b138b8
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-02 09:07:30 +0000

    Create Ticket model for contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+    sent_at = models.DateTimeField(default=timezone.now)
