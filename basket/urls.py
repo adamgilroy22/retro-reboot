22e2aaad6ab500cc583c2ef512416127a9632c9f
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-03 13:38:02 +0000

    Add url for adding discount code

--- basket/urls.py
+++ basket/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from . import contexts
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
@@ -9,4 +8,5 @@ urlpatterns = [
     path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
     path('remove/<item_id>/', views.remove_from_basket,
          name='remove_from_basket'),
+    path('add-discount/', views.add_discount, name='add-discount'),
 ]
