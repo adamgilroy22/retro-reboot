f0a95e6c5238e8b5a66db30f706178aacb317237
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-04 11:40:39 +0000

    Show modal on konami code entry and close with button

--- static/js/konami-code.js
+++ static/js/konami-code.js
@@ -1,5 +1,7 @@
 var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
 var current = 0;
+var modal = document.getElementById("konamiModal");
+let closeButton = document.querySelector(".close-button");
 
 var keyHandler = function (event) {
 
@@ -14,11 +16,23 @@ var keyHandler = function (event) {
 
 	// If complete, alert and reset
 	if (pattern.length === current) {
+		modal.style.display = "block";
 		current = 0;
-		window.alert('You found it!');
 	}
 
 };
+
+// Close modal on button click
+closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Close modal when clicking outside it
+window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+});
 
 // Listen for keydown events
 document.addEventListener('keydown', keyHandler, false);
