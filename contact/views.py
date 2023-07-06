5bd35585b6c38e87dbc057989bbf4f37ec60e0ee
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-06 15:36:49 +0000

    Create contact_form function

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Ticket
+from .forms import ContactForm
 
 
 def contact(request):
@@ -6,3 +8,25 @@ def contact(request):
     A view to return the index page
     """
     return render(request, 'contact/contact.html')
+
+
+def contact_form(request):
+    """ Handle contact form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            ticket = form.save()
+            messages.info(request, 'Message sent!')
+            return redirect(reverse('contact'))
+        else:
+            messages.error(request, 'Failed to send message. \
+            Try again.')
+    else:
+        form = ContactForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
