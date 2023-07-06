aa33738f87448a17cf29c2718f4848ae7b2e2cc1
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-06 14:22:25 +0000

    Add seen field to Ticket model

--- contact/models.py
+++ contact/models.py
@@ -3,7 +3,12 @@ from django.utils import timezone
 
 
 class Ticket(models.Model):
+    """
+    Created when a user submits a form on the contact
+    us page
+    """
     name = models.CharField(max_length=254)
     email = models.EmailField(max_length=254, null=False, blank=False)
     message = models.TextField(null=False, blank=False)
     sent_at = models.DateTimeField(default=timezone.now)
+    seen = models.BooleanField(default=False)
