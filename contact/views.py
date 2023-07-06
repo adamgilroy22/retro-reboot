2273e508193208e13e492c5b8bda3085af70d862
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-06 16:34:21 +0000

    Remove unused import

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
