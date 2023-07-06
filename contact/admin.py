5107f812d0bc5005332220822d08cb9980650cab
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-06 15:58:35 +0000

    Register Tickets to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'sent_at', 'seen')
+    search_fields = ('name', 'email', 'message')
+    actions = ['mark_seen']
+
+    def mark_seen(self, request, queryset):
+        queryset.update(seen=True)
