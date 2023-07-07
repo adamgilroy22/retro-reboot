d9feb98b8c2c783f884e9c249ed99dfca10ae1eb
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-07 10:49:43 +0000

    Add url path for closing tickets

--- contact/urls.py
+++ contact/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.contact, name='contact'),
     path('send-form', views.contact_form, name='contact_form'),
     path('open-tickets', views.ViewOpenTickets.as_view(), name='open_tickets'),
+    path('ticket/<int:pk>/close-ticket',
+         views.CloseTicket.as_view(), name='close_ticket'),
 ]
