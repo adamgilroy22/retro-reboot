49e23f3ea9b97fc1488adba60d83dc8a36095e78
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-11 18:29:52 +0000

    Add docstring for order_history function

--- profiles/views.py
+++ profiles/views.py
@@ -36,6 +36,9 @@ def profile(request):
 
 
 def order_history(request, order_number):
+    """
+    Show a user's order history on their profile
+    """
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
