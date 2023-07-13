7cbd09c5fc6de2dabe6e7bcdee8dd6ee556ecf86
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-13 16:32:39 +0000

    Calculate delivery costs in update_total function

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 from django.db.models import Sum
+from django.conf import settings
 
 from django_countries.fields import CountryField
 
@@ -45,11 +46,17 @@ class Order(models.Model):
 
     def update_total(self):
         """
-        Update grand total each time a line item is added.
+        Update grand total each time a line item is added
+        and account for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
             'lineitem_total__sum'] or 0
-        self.grand_total = self.order_total
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * \
+                settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
     def save(self, *args, **kwargs):
