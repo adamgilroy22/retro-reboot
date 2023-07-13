d94a844fd19e827aa60272dc12382a7f83684ad9
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-13 16:36:34 +0000

    Delete session discount on order completion

--- checkout/views.py
+++ checkout/views.py
@@ -158,6 +158,7 @@ def checkout_success(request, order_number):
     Handle successful checkouts
     """
     save_info = request.session.get('save_info')
+    del request.session['discount']
     order = get_object_or_404(Order, order_number=order_number)
 
     if request.user.is_authenticated:
