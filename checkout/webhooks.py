997dff8b606e2d6927453dd6a9fad86a3a40f09a
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-14 13:03:21 +0000

    Format webhooks.py

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -40,7 +40,7 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,  # noqa
     }
 
     # Get the webhook type from Stripe
