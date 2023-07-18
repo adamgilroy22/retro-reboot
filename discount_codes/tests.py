297d36ebdf5737b7be04a072ba485e87546945a4
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-18 10:47:46 +0000

    Create discount form test

--- discount_codes/tests.py
+++ discount_codes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .forms import DiscountForm
 
-# Create your tests here.
+
+class TestDiscountForm(TestCase):
+    """
+    Test if discount form is left blank when submitted
+    """
+    def test_comment_is_required(self):
+        form = DiscountForm({'code': ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn('code', form.errors.keys())
+        self.assertEqual(form.errors['code'][0], 'This field is required.')
