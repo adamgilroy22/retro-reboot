3bc06f97d0ced9e9c983f55a8a08d2186f5c68b7
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-18 13:32:40 +0000

    Test profile views

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def setUp(self):
+        """
+        Create testuser to test pages where
+        you need to be logged in to view
+        """
+        testuser = User.objects.create_user(
+            username="test_user",
+            password="test_password",
+            email="test@test.com"
+        )
+        testuser.save()
+
+    def test_profile(self):
+        """
+        Test if testuser can access profile
+        """
+        self.client.login(username="test_user", password="test_password")
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+
+    def test_profile_logged_out(self):
+        """
+        Test if logged out user gets redirected
+        when trying to access profile link
+        """
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 302)
