889e7124fc850d967aa01a7809a499e802f5c74b
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-18 11:18:59 +0000

    Adjust discount form test function name

--- discount_codes/tests.py
+++ discount_codes/tests.py
@@ -6,7 +6,7 @@ class TestDiscountForm(TestCase):
     """
     Test if discount form is left blank when submitted
     """
-    def test_comment_is_required(self):
+    def test_code_is_required(self):
         form = DiscountForm({'code': ""})
         self.assertFalse(form.is_valid())
         self.assertIn('code', form.errors.keys())
