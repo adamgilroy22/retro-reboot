697f0658e3afed06d2f5fa832f6c51a38d2d58c9
Adam Gilroy <adamgilroy22@gmail.com>
2023-07-18 13:34:15 +0000

    Test homepage view

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def test_profile(self):
+        """
+        Test if homepage loads
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'homepage/index.html')
